7fcb43f2b2aad341885d084e9c124dfc39d06fea
Inari Listenmaa <inari.listenmaa@gmail.com>
2015-11-30 17:57:43 +0100

    extracting readings for words

---
+++ Matrix.hs
@@ -0,0 +1,61 @@
+module Main where
+
+import CG_base
+import CG_Symbolic
+import Data.List
+import qualified Data.Map as M
+import qualified Data.IntMap as IM
+import qualified Data.IntSet as IS
+import System.Environment ( getArgs ) 
+
+type Key = Int
+type ExampleWordForm = String
+
+--type TagMap = M.Map Tag IS.IntSet
+type AmbClass = M.Map ExampleWordForm IS.IntSet
+
+--the IntSet from TagMap and AmbClass is the key set for LitMap
+--for TagMap, the key is a single tag and value all readings where tag appears
+--for AmbClass, the key is a word form and value all readings that the word has
+--ie. AmbClass should have more zeroes for the average key than TagMap
+--type LitMap = IM.IntMap Lit
+
+
+main = do
+ args <- getArgs
+ case args of
+  []    -> error "give a lexicon and tag combinations"
+  (w:t:_) -> do ws <- words `fmap` readFile w
+                tcs <- (map parseTC . words) `fmap` readFile t
+                let ts = concat tcs
+                let readings = map (toReadingMap tcs) ws 
+                mapM_ print readings 
+                let finalReadings = foldl group' M.empty readings 
+                print finalReadings
+
+parseTC :: String -> [Tag]
+parseTC str = map toTag $ filter (not.null) $ split isValid str
+ where 
+  isValid c = c=='<' || c=='+'
+  toTag ">>>" = BOS
+  toTag "<<<" = EOS
+  toTag []    = error "empty tag"
+  toTag str = if last str=='>' then Tag (init str) else Lem str
+
+
+group' acc (a,b) =
+  case M.lookup a acc of
+    Just c -> M.adjust (b:) a acc 
+    Nothing -> M.insert a [b] acc
+--weet:weten<vblex><imp><sg> 
+--weet:weten<vblex><imp><pl>  to 
+--weet :[ [Tag "vblex", Tag "imp", Tag "sg"]
+--      , [Tag "vblex", Tag "imp", Tag "pl"] ]
+--each of those tag combinations is a value in Word,
+--and has a key, which is a number.
+toReadingMap :: [[Tag]] -> String -> (ExampleWordForm, Key)
+toReadingMap tcs str = (weet, key)
+ where
+  (weet, vblex_imp_sg) = break (==':') str
+  tags = map Tag $ tail $ map (delete '>') $ split (=='<') vblex_imp_sg
+  Just key = elemIndex tags tcs
