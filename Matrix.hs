8e95bb51707ee0abb89761f1037b5cf21072d209
Inari Listenmaa <inari.listenmaa@gmail.com>
2016-02-08 15:16:08 +0100

    bugs with subreadings + if there's a lemma in the reading

--- Matrix.hs
+++ Matrix.hs
@@ -1,10 +1,14 @@
 module Main where
 
+import CG_base
 import Data.List
 import qualified Data.Map as M
 import qualified Data.IntMap as IM
 import qualified Data.IntSet as IS
+--import Debug.Trace
 import System.Environment ( getArgs ) 
+
+trace str a = a
 
 type Key = Int
 type ExampleWordForm = String
@@ -27,33 +31,11 @@ main = do
                 tcs <- (map parseTC . words) `fmap` readFile t
                 let ts = concat tcs
                 let readings = map (toReadingMap tcs) ws 
-                --mapM_ print (take 50 readings)
-                --mapM_ print $ M.toList $ toGraph ( readings)
-                let foo = (map) nub $ toGraph readings
-                let bar = nub foo
-                mapM_ print bar
+                mapM_ print (take 50 readings)
+                mapM_ print $ M.toList $ toGraph ( readings)
 --                mapM_ print $ toGraph readings
 
 --------------------------------------------------------------------------------
-
---weet:weten<vblex><imp><sg> 
---weet:weten<vblex><imp><pl>  to 
---"weet":[ ["vblex", "imp", "sg"]
---       , ["vblex", "imp", "pl"] ]
---each of those tag combinations is a value in Word,
---and has a key, which is a number.
-toReadingMap :: [[String]] -> String -> (ExampleWordForm, Key)
-toReadingMap tcs str = (weet, key)
- where
-  (weet, vblex_imp_sg) = break (==':') str
-  tags = tail $ map (delete '>') $ split (=='<') vblex_imp_sg
-  key = case elemIndex tags tcs of
-          Just k' -> k'
-          Nothing -> 99999
-
-
-
-
 
 --toGraph :: 
 toGraph readings = readingMap
@@ -61,8 +43,27 @@ toGraph readings = readingMap
   group' acc (a,b) = case M.lookup a acc of
                        Just c  -> M.adjust (b:) a acc 
                        Nothing -> M.insert a [b] acc
-  --readingMap = invertMap $ foldl group' M.empty readings 
-  readingMap = onlyValues 99999 $ foldl group' M.empty readings 
+  readingMap = invertMap 99999 $ foldl group' M.empty readings 
+  --readingMap = onlyValues 99999 $ foldl group' M.empty readings 
+
+
+--weet:weten<vblex><imp><sg> 
+--weet:weten<vblex><imp><pl>  to 
+--"weet":[ ["vblex", "imp", "sg"]
+--       , ["vblex", "imp", "pl"] ]
+--each of those tag combinations is a value in Word,
+--and has a key, which is a number.
+toReadingMap :: [[Tag]] -> String -> (ExampleWordForm, Key)
+toReadingMap tcs str = trace (show (weet,key)) $ (weet, key)
+ where
+  (weet, vblex_imp_sg) = break (==':') str
+  tagsList = split (=='<') vblex_imp_sg
+  tags = if null tagsList then [Tag $ delete '>' vblex_imp_sg]
+           else map Tag $ tail $ map (delete '>') tagsList
+  key = case elemIndex tags tcs of
+          Just k' -> trace "key found" $ k'
+          Nothing -> 99999
+
 
 
 onlyValues :: (Ord k, Ord v) => v -> M.Map k [v] -> [[v]]
@@ -70,19 +71,22 @@ onlyValues bad m = nub $ [vs' | (k, vs) <- M.toList m
                               , let vs' = filter (/=bad) vs
                               , not (null vs') ]
 
-invertMap :: (Ord k, Ord v) => M.Map k [v] -> M.Map [v] [k]
-invertMap m = M.fromListWith (++) pairs
-  where pairs = [(vs, [k]) | (k, vs) <- M.toList m]
+invertMap :: (Ord k, Ord v) => v -> M.Map k [v] -> M.Map [v] [k]
+invertMap bad m = M.fromListWith (++) pairs
+  where pairs = [(vs, [k]) | (k, vs) <- M.toList m
+                           , let vs' = filter (/=bad) vs
+                           , not (null vs') ]
 
-
-parseTC :: String -> [String]
-parseTC str = filter (not.null) $ split isValid str
+--TODO subreadings & lemmas
+parseTC :: String -> [Tag]
+parseTC str = map toTag $ filter (not.null) $ split isValid str
  where 
   isValid c = c=='<' || c=='+'
-{-  toTag ">>>" = BOS
+  toTag ">>>" = BOS
   toTag "<<<" = EOS
   toTag []    = error "empty tag"
-  toTag str = if last str=='>' then Tag (init str) else Lem str -}
+  toTag ('"':str) = Lem $ delete '"' str
+  toTag str = if last str=='>' then Tag (init str) else Lem str 
 
 split :: (a -> Bool) -> [a] -> [[a]]
 split p [] = []
