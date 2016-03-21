995f47f559d5f911441eee52f75caa86e5cfdb32
Inari Listenmaa <inari.listenmaa@gmail.com>
2016-03-21 15:50:01 +0100

    lot of rewriting, still doesn't compile

--- CG_SAT.hs
+++ CG_SAT.hs
@@ -3,8 +3,8 @@ module CG_SAT (
 )
 where
 
-import CG_base hiding ( Sentence, showSentence )
-import CG_base as CGB
+import CG_base hiding ( Cohort )
+--import CG_base as CGB
 import SAT ( Solver(..), newSolver, deleteSolver )
 import qualified SAT
 import SAT.Named
@@ -24,13 +24,25 @@ import Text.Regex
 --------------------------------------------------------------------------------  
 
 -- vblex |-> [vblex sg p3, vblex pl p1, vblex ]
-type TagMap   = M.Map Tag IntSet
+type TagMap   = M.Map Tag IS.IntSet
 
 type Cohort   = IM.IntMap Lit
 type Sentence = IM.IntMap Cohort
 
 --------------------------------------------------------------------------------
 
+type TrgIS    = IS.IntSet
+type DifIS    = IS.IntSet
+
+
+
+type TrgInds = (TrgIS,DifIS) --,WhoCares = all \ trg. 
+                         -- Would be needed for cautious and onlyTrgLeft.
+                         -- Question: is it better to store it or compute it for each rule?
+type CondInds = (TrgIS,DifIS)
+
+--like CG_base's Condition, but s/TagSet/CondInds/
+data Condition' = C' Position (Bool,[CondInds]) | Always' deriving (Show,Eq)
 
 data Rule' = R { trg :: TrgInds 
                , cnd :: [[Condition']]
@@ -69,34 +81,34 @@ ruleToRule' tagmap allinds rule = R trget conds isSel nm
 
 --------------------------------------------------------------------------------
 
-
--- TODO!!! Make all the apply etc. functions here; unify format between Analyse and Disambiguate!
-
 apply :: Solver -> Sentence -> Rule' -> IO Sentence
 apply s sentence rule = do
-  let ts_ds = trg rule
-  let isSel = isSelect' rule
+
+  --all possible readings that the rule targets
+  let (allTrgInds,allDifInds) = trg rule
+
 
   let applyToWord sentence (i,cohort) = do
-        let (trgLits,difLits) = lookupTag isSel ts_ds cohort -- :: (IntSet,IntSet)
-                --if Select, then these are flipped already in lookupTag
-        let allLits = M.elems cohort
-        let otherLits = allLits \\ trgLits
+       let indsInCohort   = IM.keys cohort
+       let (trgIndsRaw,otherIndsRaw) = partition (IS.member allTrgInds && IS.notMember allDifInds) indsInCohort
+       let (trgInds,otherInds) = if isSelect' rule --if Select, flip target and other
+                                  then (otherIndsRaw,trgIndsRaw)
+                                  else (trgIndsRaw,otherIndsRaw)
 
-        disjConds <- mkConds s sentence i (toConds $ cond rule)
-        case disjConds of
-          Nothing -> return sentence --conditions out of scope, no changes in sentence
-          Just cs -> do
-            
+       disjConds <- mkConds s indsInCohort sentence i (cnd rule) "apply"
+       case disjConds of
+         Nothing -> return sentence --conditions out of scope, no changes in sentence
+         Just cs -> do
+           let trgIndsList = IS.toList trgInds
 
-            condsHold <- orl' s cs
-            let trgPos   = mapMaybe   (lu' word) trgIndsList
-            let otherNeg = map (neg . lu word) (IS.toList otherInds)
+           condsHold <- orl' s cs
+           let trgPos   = mapMaybe   (lu' cohort) trgIndsList
+           let otherNeg = map (neg . lu cohort) (IS.toList otherInds)
 
-            someTrgIsTrue <- orl' s trgPos 
-            noOtherIsTrue <- andl' s otherNeg
-            onlyTrgLeft <- andl' s [ someTrgIsTrue, noOtherIsTrue ]
-            cannotApply <- orl' s [ neg condsHold, onlyTrgLeft ]
+           someTrgIsTrue <- orl' s trgPos 
+           noOtherIsTrue <- andl' s otherNeg
+           onlyTrgLeft <- andl' s [ someTrgIsTrue, noOtherIsTrue ]
+           cannotApply <- orl' s [ neg condsHold, onlyTrgLeft ]
 
            newTrgLits <- sequence
              --wN<a>' is true if both of the following:
@@ -104,7 +116,7 @@ apply s sentence rule = do
                                  , cannotApply ] --rule cannot apply 
                | oldTrgLit <- trgPos
                , let newTrgName = show oldTrgLit ++ "'" ]
-           let newsw = foldl changeAna word (zip trgIndsList newTrgLits)
+           let newsw = foldl changeAna cohort (zip trgIndsList newTrgLits)
            return $ changeWord sentence i newsw
            
   foldM applyToWord sentence (IM.assocs sentence)
@@ -116,10 +128,14 @@ apply s sentence rule = do
 
   changeWord :: Sentence -> Int -> Word -> Sentence
   changeWord sent i newsw = IM.adjust (const newsw) i sent
+
+  lu' xs x = IM.lookup x xs
+  lu  xs x = IM.findWithDefault false x xs -- neg will be called, so false will turn into true. I imagine that this is faster than call map twice?
+
 --------------------------------------------------------------------------------  
 
 --OBS. it's also perfectly fine to have an empty condition, ie. remove/select always!
-mkConds :: Solver ->  Sentence -> SIndex -> [[Condition]] -> IO (Maybe Lit)
+mkConds :: Solver ->  Sentence -> Int -> [[Condition']] -> IO (Maybe Lit)
 mkConds s  sentence trgind disjconjconds str = do
   return Nothing
   -- * conds_absinds = all possible (absolute, not relative) SIndices in range 
@@ -130,55 +146,72 @@ mkConds s  sentence trgind disjconjconds str = do
 --------------------------------------------------------------------------------  
 
 
+--We store only Ints in Sentence, but we have an [(Int,Reading)] handy 
+--when we want to print out our readings.
 
 mkSentence :: Solver -> [Reading] -> [[Reading]] -> IO Sentence
-mkSentence s allrds rdss = 
+mkSentence s allrds rdss = do
+  cohorts <- [ IM.fromList `fmap` sequence
+               [ (,) n `fmap` newLit s (show rd) | rd <- rds 
+                                                 , let n = lookup rd readingMap ] -- ::[(Int,Lit)]
+               | rds <- rdss ] -- ::[Cohort]
+  let sentence = IM.fromList $ zip [1..] cohorts
+  return sentence
+
  where
   readingMap = zip allrds [1..]
-  cohorts = [ [ newLit s  | rd <- rds ]
-              | rds <- rdss ]
-
-  showReading rd
 
 symbSentence :: Solver -> [Reading] -> Int -> IO Sentence
 symbSentence s allrds w =
   IM.fromList `fmap` sequence 
      [ ((,) n . IM.fromList) `fmap` sequence 
        [ (,) m `fmap` newLit s (showSymbReading r n m) | (m, r) <- zip [1..] allrds ]
-          n <- [1..w] ] 
+         | n <- [1..w] ] 
  where
   showSymbReading :: Reading -> Int -> Int -> String
   showSymbReading [Lem l] _ m =  l  ++ "_" ++ show m
   showSymbReading tags    i m = "w" ++ show i ++ concatMap (\t -> '<':show t++">") tags
 
+
+--------------------------------------------------------------------------------  
+
 mkTagMap :: [Tag] -> [[Tag]] -> TagMap
 mkTagMap ts tcs = M.fromList $
                    ts `for` \t -> let getInds = IS.fromList . map (1+) . findIndices (elem t)
                                   in (t, getInds tcs) 
+ where for = flip fmap
 
 
---TODO: tagmap thingy, this doesn't work!
---Make it use Rule', then we can use same functions for both Analyse and Disambiguate.
-{-
-Trg and Dif contain [[Tag]]s, which is different from Reading!!! (also a [[Tag]])
-Cohort has Readings, target has (Trg,Dif)s.
-We need to find all the readings that have Trg and don't have Dif.
-
--}
-                        
-lookupTag :: Bool -> TagMap -> Cohort -> (Trg,Dif) -> (IntSet,IntSet)
-lookupTag isSel alltags cohort (trg,dif) = 
- let allReadings = M.elems cohort :: IntSet
-     trgLits = if trg==[[]] then allReadings --this means (*)
-                else concatMap (go allReadings) trg
-     difLits = if dif==[[]] then [] --this means no - operator
-                else concatMap (go allReadings) dif
+lookupTag :: TagMap -> IS.IntSet -> (Trg,Dif) -> (IS.IntSet,IS.IntSet)
+lookupTag alltags allinds (trg,dif) = 
+  let trgInds = if trg==[[]] then allinds --this means IF (-1 (*))
+                   else IS.unions $ map (go allinds) trg --trg::[[Tag]]
+      difInds = if dif==[[]] then IS.empty --this means IF (-1 something \\ nothing)
+                      else IS.unions $ map (go allinds) dif --dif::[[Tag]]
+          
+  in  (trgInds IS.\\ difInds, difInds)
  where
-  go :: [Lit] -> [Reading] -> [Lit]
   go acc []     = acc         --default is empty set, because intersect [] _ == []
-  go acc (r:rs) = let inds = case t of
+  go acc (t:ts) = let inds = case t of
                               --(Rgx r s) -> IS.empty `fromMaybe` lookupRegex t alltags
                               _         -> IS.empty `fromMaybe` M.lookup t alltags
                   in go (IS.intersection acc inds) ts
+                       
+--lookupTag :: TagMap -> Cohort -> (Trg,Dif) -> (IS.IntSet,IS.IntSet)
+--lookupTag alltags cohort (trg,dif) = 
+-- let rdsInCohort = M.keys cohort :: IS.IntSet
+--     trgLits = if trg==[[]] then allReadings --this means (*)
+--                else concatMap (go rdsInCohort) trg
+--     difLits = if dif==[[]] then [] --this means no - operator
+--                else concatMap (go rdsInCohort) dif
+
+-- in  (trgInds IS.\\ difInds, difInds)
+-- where
+--  go :: IS.IntSet -> [Tag] -> IS.IntSet -> IS.IntSet
+--  go acc []     = acc         --default is empty set, because intersect [] _ == []
+--  go acc (t:ts) = let inds = case t of
+--                              --(Rgx r s) -> IS.empty `fromMaybe` lookupRegex t alltags
+--                              _         -> IS.empty `fromMaybe` M.lookup t alltags
+--                  in go (IS.intersection acc inds) ts
 
 
