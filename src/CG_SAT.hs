14108743ca0c16b097ab61dab110b10745bef797
Inari Listenmaa <inari.listenmaa@gmail.com>
2017-01-04 12:08:51 +0200

    add first case to ctx2Pattern

--- src/CG_SAT.hs
+++ src/CG_SAT.hs
@@ -2,15 +2,16 @@
 
 module CG_SAT where
 
-import Rule
+import Rule hiding ( Not )
+import qualified Rule as R
 import SAT ( Solver(..), newSolver, deleteSolver )
 import SAT.Named
 
 import Data.IntMap ( IntMap, (!) )
 import qualified Data.IntMap as IM
-import Data.IntSet ( IntSet, intersection, unions, difference )
-import qualified Data.Map as M
+import Data.IntSet ( IntSet, unions, union, intersection, difference )
 import Data.List ( intercalate )
+import qualified Data.Map as M
 import Data.Maybe ( catMaybes )
 
 import Control.Monad ( liftM2 )
@@ -45,12 +46,14 @@ type Cohort = IntMap Lit
 
 
 data Pattern = Pat { positions :: OrList (Seq Int)
-                   , contexts :: OrList (Seq Match) } 
+                   , contexts :: Seq Match } 
              | PatAlways deriving (Show,Eq,Ord)
 
 data Match = Mix IntSet -- Cohort contains tags in IntSet and tags not in IntSet
            | Cau IntSet -- Cohort contains only tags in IntSet
            | Not IntSet -- Cohort contains no tags in IntSet
+           | NotCau IntSet -- Cohort contains (not only) tags in IntSet:
+                            -- either of Not and Mix are valid.
            | AllTags -- Cohort matches all tags
            deriving (Show,Eq,Ord)
 
@@ -63,8 +66,10 @@ ctx2Pattern :: Int  -- ^ Sentence length
             -> CGMonad (OrList Pattern) -- ^ List of patterns. Length >1 only if the context is template.
 ctx2Pattern senlen origin ctx = case ctx of
   Always -> return $ Or [PatAlways]
-  Ctx posn polr tgst 
-    -> undefined
+  c@(Ctx posn polr tgst)
+    -> do (ps,m) <- singleCtx2Pat c
+          return $ Or [ Pat (fmap (\x -> Seq [x]) ps) (Seq [m]) ]
+          
   Link ctxs 
     -> undefined
   Template ctxs
@@ -72,36 +77,44 @@ ctx2Pattern senlen origin ctx = case ctx of
   Negate ctx -> undefined
 
  where 
-  singleCtx2Pattern (Ctx posn polr tgst) = undefined
+  singleCtx2Pat (Ctx posn polr tgst) = 
+    do tagset <- normaliseAbs tgst
+       let allPositions = pos2inds posn senlen origin
+       let match = maybe AllTags (getMatch posn polr) tagset
+       return (allPositions,match) 
+
+pos2inds :: Position -> Int -> Int -> OrList Int
+pos2inds posn senlen origin = 
+  case scan posn of 
+    Exactly -> Or [origin + relInd]
+    _       -> Or absInds
+ where
+  relInd = R.pos posn
+  absInds = if relInd<0 then [1 .. origin+relInd]
+                else [origin+relInd .. senlen]
+
+getMatch :: Position -> Polarity -> (IntSet -> Match)
+getMatch (Pos _ NC _) Yes = Mix
+getMatch (Pos _ NC _) R.Not = Not
+getMatch (Pos _ C _)  Yes = Cau
+getMatch (Pos _ C _)  R.Not = NotCau
 
 
-
-lu :: OrList Reading -> M.Map Tag IntSet -> IntSet
-lu s m = unions [ foldl1 intersection $ 
-                   catMaybes [ M.lookup t m | t <- getAndList ts ]
-                    | ts <- getOrList s ]
-
+-- | Takes a tagset, with OrLists of underspecified readings,
+-- and returns corresponding IntSets of fully specified readings.
+-- Compare to normaliseRel in cghs/Rule: it only does the set operations
+-- relative to the underspecified readings, not with absolute IntSets.
+-- That's why we cannot handle Diffs in normaliseRel, but only here.
 normaliseAbs :: TagSet -> CGMonad (Maybe IntSet)
 normaliseAbs tagset = case tagset of
-  All -> return Nothing
+  All   -> return Nothing
 
-  Set s {- For example: Set (Or [And [vblex, sg], And [n, pl]])
-            ( Trg (Or [ IS(321,322,323) `intersect` IS(1,2,3,...,321,323) 
-                      , IS(2,3,50,450) `intersect` IS(4,5,6,50,1908) ])
-            , Dif mempty )
-          -}
-    -> Just `fmap` lu s `fmap` asks tagMap
+  Set s -> Just `fmap` lu s `fmap` asks tagMap
 
-
-  Union t t' {- For example: Union (List (Or [And [vblex])
-                                   (Diff (List (Or [And [vbser]) 
-                                         (List (Or [And [aux]]) )
-                This becomes two TD in the OrList: 
-                Or [(Trg vblex, Dif mempty), (Trg vbser, Dif aux)].
-                To match this OrList of TDs, a cohort has to match either. -}
-    -> do is  <- normaliseAbs t
-          is' <- normaliseAbs t'
-          return undefined
+  Union t t' 
+        -> do is  <- normaliseAbs t
+              is' <- normaliseAbs t'
+              return $ liftM2 union is is'
   Inters t t'
     -> do is  <- normaliseAbs t
           is' <- normaliseAbs t'
@@ -110,17 +123,30 @@ normaliseAbs tagset = case tagset of
 -- Intended behaviour:
 --   adv adV (ada "very") `diff` ada == adv adV
 --   adv adV ada `diff` (ada "very") == adv adV (ada xxx) (ada yyy) ... 
--- This will work automatically now: normaliseAbs returns intsets,
--- and `lookup ada tagmap` returns all: (ada xxx), (ada yyy), (ada "very")
--- Then, the difference with t' will only match (ada "very").
+-- This works properly now: normaliseAbs returns IntSets, 
+-- and `normaliseAbs (ada)` returns all of (ada xxx), (ada yyy), (ada "very"), ...
+-- Then, the difference with `normaliseAbs (adv adV (ada "very")) will only exclude (ada "very").
   Diff t t' 
     -> do is  <- normaliseAbs t
           is' <- normaliseAbs t'
           return $ liftM2 difference is is'
 
-      
-  Cart ts ts' 
-    -> undefined
+-- say t=[n,adj] and t'=[m,f,mf]
+-- is  = specified readings that match t: (n foo), (n bar), (n mf), ... , (adj foo), (adj bar), (adj f), ..
+-- is' = specified readings that match t': (n m), (n f), (vblex sg p3 mf), (adj f), ..
+-- intersection of the specified readings returns those that match 
+-- sequence [[n,adj],[m,f,mf]]: (n m), (n f), ..., (adj f), (adj mf)
+  Cart t t' 
+    -> do is  <- normaliseAbs t
+          is' <- normaliseAbs t'
+          return $ liftM2 intersection is is'
+
+
+ where
+  lu :: OrList Reading -> M.Map Tag IntSet -> IntSet
+  lu s m = unions [ foldl1 intersection $ 
+                     catMaybes [ M.lookup t m | t <- getAndList ts ]
+                      | ts <- getOrList s ]
 
 {-
 Make this only once, at the beginning:
@@ -149,4 +175,4 @@ many times per rule --- once for each tagset.
 newtype Seq a = Seq { getSeq :: [a] } deriving (Eq,Ord,Functor,Foldable,Monoid)
 
 instance (Show a) => Show (Seq a) where
-  show = intercalate ">" . map show . getSeq
+  show = intercalate "," . map show . getSeq
