35678fd787ff9592693e3babfe98e59f6600d964
Inari Listenmaa <inari.listenmaa@gmail.com>
2017-02-08 17:13:33 +0100

    use the results of triggers function. TODO: I'm not sure if all the concats actually do the right thing

--- src/Analyse.hs
+++ src/Analyse.hs
@@ -13,6 +13,7 @@ import CGHS
 
 import Data.Foldable ( fold )
 import qualified Data.IntMap as IM
+import qualified Data.Map as M
 import Data.Either ( lefts, rights )
 import Data.List ( nub, elemIndex )
 import Data.Maybe ( catMaybes, fromMaybe )
@@ -29,16 +30,16 @@ data Conflict = NoConf | Internal | Interaction -- [Rule]
 ----------------------------------------------------------------------------
 -- Functions that apply only for analysis, not disambiguation
 
-testRules :: [Rule] -> RWSE [Conflict]
-testRules = mapM testRule
+testRules :: Bool -> [Rule] -> RWSE [Conflict]
+testRules v = mapM (testRule v)
 
 
-testRule :: Rule -> RWSE Conflict
-testRule rule = do
+testRule :: Bool -> Rule -> RWSE Conflict
+testRule v rule = do
   c@(Config len sen) <- get
   e@(Env w l r s) <- ask
   confs <-    -- I suspect there's a nicer way to handle this
-     mapM (\i -> RWSE $ lift $ runExceptT $ runRWSE $ ruleTriggers False rule i
+     mapM (\i -> RWSE $ lift $ runExceptT $ runRWSE $ ruleTriggers v rule i
                   :: RWSE (Either CGException Conflict) )
                 [1..len] -- 1) Test if the rule may apply, and return result of that
 
@@ -59,9 +60,10 @@ ruleTriggers :: Bool -> Rule -> Int -> RWSE Conflict
 ruleTriggers verbose rule i = do
   s <- asks solver
   (Config len sen) <- get
-  (allCondsHold, trgAndOthers) <- trigger rule i
-  mustHaveTrg <- liftIO $ orl' s [true] -- TODO: excract lits from TargetCohort
-  mustHaveOther <- liftIO $ orl' s (map snd trgAndOthers)
+  (allCondsHold, trgCohs_otherLits) <- trigger rule i
+  let (trgCohs, otherLits) = unzip trgCohs_otherLits
+  mustHaveTrg <- liftIO $ orl' s (concatMap getLits trgCohs) --TODO
+  mustHaveOther <- liftIO $ orl' s otherLits --TODO
   b <- liftIO $ solve s [allCondsHold]
   if b then do when verbose $
                  liftIO $ solveAndPrint True s [mustHaveTrg, mustHaveOther, allCondsHold] sen
@@ -73,15 +75,18 @@ ruleTriggers verbose rule i = do
                                            tempConf <- mkConfig len
                                            put tempConf
                                            tempSen <- gets sentence
-                                           --(x,y,z,_,_) <- trigger rule i
-                                           let (x,y,z) = (true,true,true)
-                                           b <- liftIO $ solve s' [x,y,z]
+                                           liftIO $ defaultRules s' tempSen
+                                           (condsHold,trg_oth) <- trigger rule i
+                                           let (trg,oth) = unzip trg_oth --TODO
+                                           mustHaveTrg <- liftIO $ orl' s' (concatMap getLits trg)
+                                           mustHaveOth <- liftIO $ orl' s' oth
+                                           b <- liftIO $ solve s' [condsHold,mustHaveTrg,mustHaveOth]
                                            if b then do when verbose $ 
-                                                          liftIO $ solveAndPrint True s' [x,y,z] tempSen
+                                                          liftIO $ solveAndPrint True s' [condsHold,mustHaveTrg,mustHaveOth] tempSen
                                                         return Interaction
                                            else do when verbose $ do
                                                      liftIO $ print rule
-                                                     liftIO $ solveAndPrint True s' [x,y,z] tempSen
+                                                     liftIO $ solveAndPrint True s' [condsHold,mustHaveTrg,mustHaveOth] tempSen
                                                    return Internal
         liftIO $ deleteSolver s'
         put (Config len sen)
@@ -94,3 +99,11 @@ width rule = (length [minw..maxw], maybe 9999 (1+) (elemIndex 0 [minw..maxw]))
   ctxScopes = fmap scopes (context rule) :: AndList (OrList Int) -- And [Or [1], Or [1,2,3], Or [-2,-1]]
   flatScopes = fold (getAndList ctxScopes) :: OrList Int -- Or [1,1,2,3,-2,-1]
   (minw,maxw) = (0 `min` minimum flatScopes, 0 `max` maximum flatScopes)
+
+
+getLits :: TargetCohort -> [Lit]
+getLits (Coh x y z) = xlits++ylits++zlits
+ where
+  xlits = M.elems x
+  ylits = M.elems y
+  zlits = M.elems z
